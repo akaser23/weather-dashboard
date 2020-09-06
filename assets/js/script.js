701c6bb84ac113be17bc211763c0cb1c15d27b21
Anna Kaser <kaserann96@gmail.com>
2020-09-06 12:25:21 -0600

    style, weather icons, uv index

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,8 +21,7 @@ var humidTwo = document.querySelector("#humid2");
 var humidThree = document.querySelector("#humid3");
 var humidFour = document.querySelector("#humid4");
 var humidFive = document.querySelector("#humid5");
-
-// var uvIndexEl = document.querySelector("#uv");
+var uvIndexEl = document.querySelector("#uv");
 var citySearch = [];
 
 var getCityWeather = function (city) {
@@ -35,49 +34,29 @@ var getCityWeather = function (city) {
             //request was successful
             if (response.ok) {
                 response.json().then(function (data) {
+                    console.log(data);
                     displayWeather(data);
+
+                    var lat = data.coord.lat;
+                    var lon = data.coord.lon;
+                    
+                    fetch("http://api.openweathermap.org/data/2.5/uvi?appid=c0acf9a2d1becd72a92787658f4ca1e7&lat="
+                    + lat 
+                    + "&lon="
+                    +lon)
+                    .then(function(response) {
+                        if(response.ok) {
+                            response.json().then(function(data) {
+                                displayUv(data.value);
+                            })
+                        }
+                    });
                 });
             } else {
                 alert("Error: " + response.statusText);
             }
         })
 };
-
-// var getCityWeather = function (city) {
-//     //format the github api url
-//     var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=c0acf9a2d1becd72a92787658f4ca1e7"
-
-//     //make request to the url
-//     fetch(apiUrl)
-//         .then(function (response) {
-//             //request was successful
-//             if (response.ok) {
-//                 response.json().then(function (data) {
-//                     displayWeather(data);
-//                     console.log(data);
-//                 });
-//             } else {
-//                 alert("Error: " + response.statusText);
-//             }
-//         }).then(function(data) {
-//             //store the lat lon to a variable
-//             var lat = response.coord.lat
-//             var lon = response.coord.lon
-            
-//             return fetch ("http://api.openweathermap.org/data/2.5/uvi/forecast?appid=c0acf9a2d1becd72a92787658f4ca1e7&lat=" + lat + "&lon=" + lon + "&cnt=1");
-
-//         }).then(function (response) {
-//             if (response.ok) {
-//                 return response.json();
-//             } else {
-//                 return Promise.reject (response);
-//             }
-//         }).then(function(lat) {
-//             console.log(data, lat);
-//         }).then(function(lon) {
-//             console.log(data.lon);
-//         });
-// };
 
 var getFiveDay = function (city) {
 
@@ -121,7 +100,7 @@ var searchHistory = function (cityName) {
 
     histEl.setAttribute("onclick", "searchHistoryWeather(this);");
 
-    cityHistoryEl.appendChild(histEl);
+    cityHistoryEl.prepend(histEl);
 };
 
 var searchHistoryWeather = function(ele) {
@@ -133,30 +112,69 @@ var searchHistoryWeather = function(ele) {
 var displayWeather = function (weather) {
     citySearchTerm.innerHTML = weather.name + " " + moment().format("MM[/]DD[/]YYYY");
 
-    tempEl.innerHTML = weather.main.temp;
+    var iconcode = weather.weather[0].icon;
+    var iconUrl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+    $('#icon').attr('src', iconUrl);
+
+    tempEl.innerHTML = Math.round((weather.main.temp - 273.15) * (9/5) + 32) + "&deg; F";
 
     humidityEl.innerHTML = weather.main.humidity + "%";
 
     windEl.innerHTML = weather.wind.speed + " MPH";
-
-    // uvIndexEl.innerHTML = weather
 }
+
+var displayUv = function (uvIndex) {
+
+    uvIndexEl.innerHTML = uvIndex;
+    $(uvIndexEl).removeClass("low").removeClass("moderate").removeClass("high");
+
+    if (uvIndex < 4) {
+        $(uvIndexEl).addClass("low");
+    }
+    else if (uvIndex < 8 && uvIndex > 4) {
+        $(uvIndexEl).addClass("moderate"); 
+    }
+    else {
+        $(uvIndexEl).addClass("high");  
+    }
+
+};
 
 var displayFiveDay = function (weatherForecast) {
     
     console.log(weatherForecast.list);
 
-    dateOne.innerHTML = weatherForecast.list[4].dt_txt;
-    dateTwo.innerHTML = weatherForecast.list[12].dt_txt;
-    dateThree.innerHTML = weatherForecast.list[20].dt_txt;
-    dateFour.innerHTML = weatherForecast.list[28].dt_txt;
-    dateFive.innerHTML = weatherForecast.list[36].dt_txt;
+    dateOne.innerHTML = weatherForecast.list[4].dt_txt.slice(0,10);
+    dateTwo.innerHTML = weatherForecast.list[12].dt_txt.slice(0,10);
+    dateThree.innerHTML = weatherForecast.list[20].dt_txt.slice(0,10);
+    dateFour.innerHTML = weatherForecast.list[28].dt_txt.slice(0,10);
+    dateFive.innerHTML = weatherForecast.list[36].dt_txt.slice(0,10);
 
-    tempOne.innerHTML = weatherForecast.list[4].main.temp;
-    tempTwo.innerHTML = weatherForecast.list[12].main.temp;
-    tempThree.innerHTML = weatherForecast.list[20].main.temp;
-    tempFour.innerHTML = weatherForecast.list[28].main.temp;
-    tempFive.innerHTML = weatherForecast.list[36].main.temp;
+    var iconcodeOne = weatherForecast.list[4].weather[0].icon;
+    var iconUrlOne = "http://openweathermap.org/img/w/" + iconcodeOne + ".png";
+    $('#icon-one').attr('src', iconUrlOne);
+
+    var iconcodeTwo = weatherForecast.list[12].weather[0].icon;
+    var iconUrlTwo = "http://openweathermap.org/img/w/" + iconcodeTwo + ".png";
+    $('#icon-two').attr('src', iconUrlTwo);
+
+    var iconcodeThree = weatherForecast.list[20].weather[0].icon;
+    var iconUrlThree = "http://openweathermap.org/img/w/" + iconcodeThree + ".png";
+    $('#icon-three').attr('src', iconUrlThree);
+
+    var iconcodeFour = weatherForecast.list[28].weather[0].icon;
+    var iconUrlFour = "http://openweathermap.org/img/w/" + iconcodeFour + ".png";
+    $('#icon-four').attr('src', iconUrlFour);
+
+    var iconcodeFive = weatherForecast.list[36].weather[0].icon;
+    var iconUrlFive = "http://openweathermap.org/img/w/" + iconcodeFive + ".png";
+    $('#icon-five').attr('src', iconUrlFive);
+
+    tempOne.innerHTML = Math.round((weatherForecast.list[4].main.temp - 273.15) * (9/5) + 32) + "&deg; F";
+    tempTwo.innerHTML = Math.round((weatherForecast.list[12].main.temp - 273.15) * (9/5) + 32) + "&deg; F";
+    tempThree.innerHTML = Math.round((weatherForecast.list[20].main.temp - 273.15) * (9/5) + 32) + "&deg; F";
+    tempFour.innerHTML = Math.round((weatherForecast.list[28].main.temp - 273.15) * (9/5) + 32) + "&deg; F";
+    tempFive.innerHTML = Math.round((weatherForecast.list[36].main.temp - 273.15) * (9/5) + 32) + "&deg; F";
 
     humidOne.innerHTML = weatherForecast.list[4].main.humidity + "%";
     humidTwo.innerHTML = weatherForecast.list[12].main.humidity + "%";
